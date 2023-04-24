416328f4ee0a2842aa251277045451a659d53a57
shanmukharao369 <115773678+shanmukharao369@users.noreply.github.com>
2023-04-24 20:44:45 +0530

    Login tsk-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
-import SignUpPage from "./Components/SignUp";
+import SignUpPage from "./Components/SignUpPage";
+import LoginPage from "./Components/LoginPage";
+import AfterLogin from "./Components/AfterLogin";
+import {BrowserRouter as Router, Route , Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <SignUpPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <SignUpPage />
+        </Route>
+        <Route exact path="/LoginPage">
+          <LoginPage />
+        </Route>
+        <Route exact path="/WelcomePage">
+          <AfterLogin />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
