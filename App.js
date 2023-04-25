f44bbb3b2417881911a80474c8823559eaf96837
shanmukharao369 <115773678+shanmukharao369@users.noreply.github.com>
2023-04-25 19:47:34 +0530

    Logout User tsk-6

---
+++ App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import SignUpPage from "./Components/SignUpPage";
+import LoginPage from "./Components/LoginPage";
+import AfterLogin from "./Components/AfterLogin";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import FirstPageDetails from "./Components/FirstPage";
+import CompleteProfile from "./Components/CompleteProfile";
+import VerificationPage from "./Components/VerificationPage";
+
+function App() {
+  return (
+    <Router>
+      <FirstPageDetails />
+      <Switch>
+        <Route exact path="/SignupPage">
+          <SignUpPage />
+        </Route>
+        <Route exact path="/LoginPage">
+          <LoginPage />
+        </Route>
+        <Route exact path="/WelcomePage">
+          <AfterLogin />
+        </Route>
+        <Route exact path="/completeProfile">
+          <CompleteProfile />
+        </Route>
+        <Route exact path="/Home">
+          <Redirect to={"/LoginPage"} />
+        </Route>
+        <Route exact path="/verify-email">
+          <VerificationPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
